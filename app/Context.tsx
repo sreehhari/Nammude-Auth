5c9f042d1c06c7fe618e50fa3e1e5dc238a5c24b
Sreehari Sujith M R <sreehari9300@gmail.com>
2025-10-14 23:45:59 +0530

    working qr scanner

--- app/Context.tsx
+++ app/Context.tsx
@@ -1,32 +1,40 @@
-"use client"
+"use client";
 
-import React, { createContext, ReactNode, useContext, useMemo, useState } from 'react'
+import React, {
+  createContext,
+  ReactNode,
+  useContext,
+  useMemo,
+  useState,
+} from "react";
 //define the shape of the context data ie define the main interface
-interface ScannerContextType{
-    isScannerOpen:boolean;
-    openScanner:()=> void;
-    closeScanner:()=>void;
-    setScanSuccessHandler:(handler:(decodedText:string)=>void)=>void;
-    triggerScanSuccess:(decodedText:string)=>void;
+interface ScannerContextType {
+  isScannerOpen: boolean;
+  openScanner: () => void;
+  closeScanner: () => void;
+  setScanSuccessHandler: (handler: (decodedText: string) => void) => void;
+  triggerScanSuccess: (decodedText: string) => void;
 }
 
-const ScannerContext = createContext<ScannerContextType|undefined>(undefined);
+const ScannerContext = createContext<ScannerContextType | undefined>(undefined);
 
-const ScannerProvider = ({children}:{children:ReactNode}) => {
-  const[isScannerOpen,setIsScannerOpen]=useState(false);
-  const[onScanSuccess,setOnScanSuccess]=useState<(decodedText:string)=>void>(()=>()=>{});
-  const openScanner = ()=>setIsScannerOpen(true);
-  const closeScanner =()=>setIsScannerOpen(false);
+const ScannerProvider = ({ children }: { children: ReactNode }) => {
+  const [isScannerOpen, setIsScannerOpen] = useState(false);
+  const [onScanSuccess, setOnScanSuccess] = useState<
+    (decodedText: string) => void
+  >(() => () => {});
+  const openScanner = () => setIsScannerOpen(true);
+  const closeScanner = () => setIsScannerOpen(false);
 
-  const setScanSuccessHandler=(handler:(decodedText:string)=>void)=>{
-    setOnScanSuccess(()=>handler);
+  const setScanSuccessHandler = (handler: (decodedText: string) => void) => {
+    setOnScanSuccess(() => handler);
   };
 
-  const triggerScanSuccess =(decodedText:string)=>{
-    if(onScanSuccess){
+  const triggerScanSuccess = (decodedText: string) => {
+    if (onScanSuccess) {
       onScanSuccess(decodedText);
     }
-  }
+  };
   // const value ={
   //   isScannerOpen,
   //   openScanner,
@@ -34,27 +42,32 @@ const ScannerProvider = ({children}:{children:ReactNode}) => {
   //   setScanSuccessHandler,
   //   triggerScanSuccess,
   // }
-  const value = useMemo(()=>({
-    isScannerOpen,
-    openScanner,
-    closeScanner,
-    setScanSuccessHandler,
-    triggerScanSuccess
-  }),[isScannerOpen,openScanner,closeScanner,setScanSuccessHandler,triggerScanSuccess])
-  return (
-    <ScannerContext.Provider value={value}>
-      {children}
-    </ScannerContext.Provider>
+  const value = useMemo(
+    () => ({
+      isScannerOpen,
+      openScanner,
+      closeScanner,
+      setScanSuccessHandler,
+      triggerScanSuccess,
+    }),
+    [
+      isScannerOpen,
+      openScanner,
+      closeScanner,
+      setScanSuccessHandler,
+      triggerScanSuccess,
+    ]
   );
-}
-export const useScanner =()=>{
+  return (
+    <ScannerContext.Provider value={value}>{children}</ScannerContext.Provider>
+  );
+};
+export const useScanner = () => {
   const context = useContext(ScannerContext);
-  if(context==undefined){
-    throw new Error('useScanner must be used within a scanner provider ');
-    
+  if (context === undefined) {
+    throw new Error("useScanner must be used within a scanner provider ");
   }
   return context;
+};
 
-}
-
-export default ScannerProvider
+export default ScannerProvider;
